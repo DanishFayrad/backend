57a99fc03a5e41cf0d95e7606adbe047c424f06d
BilalAulakh <bb807562@gmail.com>
2026-04-24 20:27:03 +0500

    ajd

--- src/routes/signalRoutes.js
+++ src/routes/signalRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 import * as signalController from '../controllers/signalController.js';
 import { requireAuth, requireAdmin } from '../middleware/authMiddleware.js';
 import { upload } from '../middleware/uploadMiddleware.js';
+// Global Timer Routes
+router.post('/global-timer', [requireAuth, requireAdmin], signalController.setGlobalTimer);
+router.get('/global-timer', requireAuth, signalController.getGlobalTimer);
+router.delete('/global-timer', [requireAuth, requireAdmin], signalController.clearGlobalTimer);
+
 router.get('/dashboard', requireAuth, signalController.getSignalsDashboard);
 router.post('/create', [requireAuth, requireAdmin, upload.single('signal_image')], signalController.createSignal);
 router.post('/take', requireAuth, signalController.takeSignal);
@@ -10,10 +15,6 @@ router.get('/user/:user_id/history', requireAuth, signalController.getUserSignal
 router.post('/:id/extend-timer', [requireAuth, requireAdmin], signalController.extendTimer);
 router.delete('/:id', requireAuth, signalController.deleteSignal);
 
-// Global Timer Routes
-router.post('/global-timer', [requireAuth, requireAdmin], signalController.setGlobalTimer);
-router.get('/global-timer', requireAuth, signalController.getGlobalTimer);
-router.delete('/global-timer', [requireAuth, requireAdmin], signalController.clearGlobalTimer);
 
 // Signal Request Routes
 router.post('/request-access', requireAuth, signalController.requestSignalAccess);
